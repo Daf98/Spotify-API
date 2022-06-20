fbf6f9f3d3bb758e8f2c319a44fa00aa30f42acc
Daf <dafne.azzolina@gmail.com>
2022-06-20 20:56:42 -0300

    Get Spotify token

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './style.css';
+const Buffer = require('buffer/').Buffer;
+
+const clientID = '54e1c1ed18694a4783e400e6647c8109';
+const clientSecret = 'd88c4932b8cb456e976aeaedb74f4a42';
+
+//async function to get the token
+const getToken = async() => {
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+        'Authorization': 'Basic ' + Buffer.from(clientID + ':' + clientSecret).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    })
+    const data = await result.json();
+    const realToken = data.access_token;
+    return realToken;
+}
+getToken().then((token) => {
+    //async function to get songs
+const getTracks = async(whatever) => {
+    const result = await fetch('https://api.spotify.com/v1/track/6ydEhrdfzhI29D2NBAqUY1', {
+        method: 'GET',
+        headers: {
+        'Authorization': 'Bearer ' + whatever
+    }
+    });
+    const data = await result.json();
+    return data;
+}
+getTracks(token);
+
+});
+
+
