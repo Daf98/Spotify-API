221814dc0a2a3d22bd46ab282b59b6cac6e76773
Fabien <fabienswift@gmail.com>
2022-06-23 21:05:20 -0400

    Fix errors

--- src/spotifyAPI.js
+++ src/spotifyAPI.js
@@ -1,28 +1,28 @@
-import './style.css';
-import image from './spotitfy-img.png';
-import newComment from './newcomment.js';
-import getComment from './getcomment.js';
+import "./style.css";
+import image from "./spotitfy-img.png";
+import newComment from "./newcomment.js";
+import getComment from "./getcomment.js";
 
-const { Buffer } = require('buffer/');
+const { Buffer } = require("buffer/");
 
 const spotifyAPI = () => {
   // add logo
-  const logo = document.getElementById('logo-div');
+  const logo = document.getElementById("logo-div");
   logo.innerHTML = `<img id="logo" src=${image} alt="logo" />`;
 
-  const clientID = '54e1c1ed18694a4783e400e6647c8109';
-  const clientSecret = 'd88c4932b8cb456e976aeaedb74f4a42';
+  const clientID = "54e1c1ed18694a4783e400e6647c8109";
+  const clientSecret = "d88c4932b8cb456e976aeaedb74f4a42";
 
   // async function to get the token
   const getToken = async () => {
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      body: 'grant_type=client_credentials',
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      body: "grant_type=client_credentials",
       headers: {
         Authorization: `Basic ${Buffer.from(
-          `${clientID}:${clientSecret}`,
-        ).toString('base64')}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
+          `${clientID}:${clientSecret}`
+        ).toString("base64")}`,
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
     const data = await result.json();
@@ -32,13 +32,13 @@ const spotifyAPI = () => {
     // async function to get songs
     const getTracks = async () => {
       const result = await fetch(
-        'https://api.spotify.com/v1/tracks?market=CU&ids=2aoo2jlRnM3A0NyLQqMN2f%2C3uz0O62HqYoyRiWZjS61KK%2C5ghIJDpPoe3CfHMGu71E6T%2C57JVGBtBLCfHw2muk5416J%2C70LcF31zb1H0PyJoS1Sx1r%2C3dPQuX8Gs42Y7b454ybpMR',
+        "https://api.spotify.com/v1/tracks?market=CU&ids=2aoo2jlRnM3A0NyLQqMN2f%2C3uz0O62HqYoyRiWZjS61KK%2C5ghIJDpPoe3CfHMGu71E6T%2C57JVGBtBLCfHw2muk5416J%2C70LcF31zb1H0PyJoS1Sx1r%2C3dPQuX8Gs42Y7b454ybpMR",
         {
-          method: 'GET',
+          method: "GET",
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        },
+        }
       );
       const data = await result.json();
       return data;
@@ -50,7 +50,7 @@ const spotifyAPI = () => {
         console.log(trackArray);
         const trackName = track.name;
         const albumImg = track.album.images[0].url;
-        const body = document.getElementById('main');
+        const body = document.getElementById("main");
         body.innerHTML += `<section class="song">
         <h2><img id="album-img" src="${albumImg}" alt="album cover"></h2>
         <h2 id="id-container"><i class="fa-solid fa-heart"></i></h2>
@@ -58,14 +58,14 @@ const spotifyAPI = () => {
         <button class="comments">Comments</button>
         </section>
         `;
-        const mainBody = document.querySelector('body');
-        const active = document.querySelectorAll('.comments');
+        const mainBody = document.querySelector("body");
+        const active = document.querySelectorAll(".comments");
         for (let i = 0; i < active.length; i += 1) {
-          active[i].addEventListener('click', () => {
-            const section = document.createElement('section');
-            section.className = 'popup';
-            const article = document.createElement('article');
-            article.className = 'pop-window';
+          active[i].addEventListener("click", () => {
+            const section = document.createElement("section");
+            section.className = "popup";
+            const article = document.createElement("article");
+            article.className = "pop-window";
             article.innerHTML = `<h2 class="img"><img class="pop-img" src="${trackArray[i].album.images[0].url}" alt="album cover"></h2>
             <h2 class="close-pop"><i class="fa-solid fa-x"></i></h2>
             <div class="description"><h2>${trackArray[i].name}</h2>
@@ -99,10 +99,10 @@ const spotifyAPI = () => {
             `;
             section.appendChild(article);
             mainBody.appendChild(section);
-            const userName = document.getElementById('name');
-            const userComment = document.getElementById('txt');
-            const comment = document.querySelector('.form');
-            comment.addEventListener('submit', (e) => {
+            const userName = document.getElementById("name");
+            const userComment = document.getElementById("txt");
+            const comment = document.querySelector(".form");
+            comment.addEventListener("submit", (e) => {
               e.preventDefault();
               getComment(track.id).then((comments) => {
                 comments.forEach((comment) => {
@@ -110,13 +110,13 @@ const spotifyAPI = () => {
                   const userC = comment.comment;
                   const date = comment.creation_date;
                   const commentList = `<li class="score-list">${date}${userN}:${userC}</li>`;
-                  document.getElementById('commenter').innerHTML += commentList;
+                  document.getElementById("commenter").innerHTML += commentList;
                 });
               });
               newComment(track.id, userName.value, userComment.value);
             });
-            const popclose = document.querySelector('.fa-x');
-            popclose.addEventListener('click', () => {
+            const popclose = document.querySelector(".fa-x");
+            popclose.addEventListener("click", () => {
               section.remove(article);
             });
             getComment(track.id).then((comments) => {
@@ -125,19 +125,16 @@ const spotifyAPI = () => {
                 const userC = comment.comment;
                 const date = comment.creation_date;
                 const commentList = `<li class="score-list">${date}${userN}:${userC}</li>`;
-                document.getElementById('commenter').innerHTML += commentList;
+                document.getElementById("commenter").innerHTML += commentList;
               });
             });
           });
         }
-        const likeButton = document.querySelectorAll('.fa-heart');
+        const likeButton = document.querySelectorAll(".fa-heart");
         likeButton.forEach((button) => {
-          button.addEventListener('click', () => {
+          button.addEventListener("click", () => {
             // newLike(track.id);
           });
-        });
-        likeButton.forEach((button) => {
-          button.addEventListener('click'); // getLike);
         });
       });
     });
